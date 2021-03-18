8fc2ce588af3c77cd1a0c909af85a2ed12755969
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2021-03-18 15:47:19 +0100

    Detect key presses when game is focused

--- project/src/main/java/tjueførtiåtte/fxui/GameController.java
+++ project/src/main/java/tjueførtiåtte/fxui/GameController.java
@@ -1,5 +1,32 @@
 package tjueførtiåtte.fxui;
 
-public class GameController {
+import javafx.fxml.FXML;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import tjueførtiåtte.model.Game;
 
+public class GameController {
+	private Game game;
+	
+	@FXML Pane gamePane;
+	
+	@FXML
+	private void initialize() {
+		game = new Game();
+		System.out.println("init");
+	}
+
+	@FXML
+	private void keyPressed(KeyEvent keyEvt) {
+		System.out.println("key:");
+		System.out.println(keyEvt.getCode().toString());
+		keyEvt.consume();
+	}
+	
+	@FXML
+	private void gameClicked(MouseEvent evt) {
+		gamePane.requestFocus();
+	}
+	
 }
