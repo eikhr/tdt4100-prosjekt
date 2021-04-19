8c6069558c8b150741809a30d9382c0050c8c5dc
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2021-04-19 10:39:47 +0200

    Improve position construction validation

--- project/src/main/java/tjueførtiåtte/model/Position.java
+++ project/src/main/java/tjueførtiåtte/model/Position.java
@@ -6,10 +6,17 @@ public class Position {
 	private Board board;
 	
 	public Position(Board board, int x, int y) {
+		validateBoard(board);
 		validatePosition(board, x, y);
 		this.board = board;
 		this.x = x;
 		this.y = y;
+	}
+	
+	public void validateBoard(Board board) {
+		if (board == null) {
+			throw new IllegalArgumentException("A position must be on a board");
+		}
 	}
 	
 	public void validatePosition(Board board, int x, int y) {
