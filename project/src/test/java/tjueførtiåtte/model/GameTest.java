ab17659374dbc31aae33ca7a67efa72b7df79774
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2021-04-25 21:25:36 +0200

    Remove unused import

--- project/src/test/java/tjueførtiåtte/model/GameTest.java
+++ project/src/test/java/tjueførtiåtte/model/GameTest.java
@@ -1,7 +1,6 @@
 package tjueførtiåtte.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Collection;
